57f50aba7a98b4b15cfb01291fd764d06993c086
devilltt <tt416926063@qq.com>
2019-01-29 08:49:59 +0800

    map.containsKey();map.containsValue();统计map中是否包含某键或某值

---
+++ src/com/tt/chapter11/hoding/PetMap.java
@@ -0,0 +1,23 @@
+package com.tt.chapter11.hoding;
+
+import com.tt.chapter14.typeinfo.pets.Cat;
+import com.tt.chapter14.typeinfo.pets.Dog;
+import com.tt.chapter14.typeinfo.pets.Hamster;
+import com.tt.chapter14.typeinfo.pets.Pet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PetMap {
+    public static void main(String[] args) {
+        Map<String, Pet> petMap = new HashMap<>();
+        petMap.put("My Cat", new Cat("Molly"));
+        petMap.put("My Dog", new Dog("Ginger"));
+        petMap.put("My Hamster", new Hamster("Bosco"));
+        System.out.println(petMap);
+        Pet dog = petMap.get("My Dog");
+        System.out.println(dog);
+        System.out.println(petMap.containsKey("My dog"));
+        System.out.println(petMap.containsValue(dog));
+    }
+}
