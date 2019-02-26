484ac7df966e1d0816e40e8075430b25f9ef0307
deviltt <tt416926063@qq.com>
2019-02-26 20:22:08 +0800

    针对接口编程，提高程序的可扩展性

---
+++ src/com/tt/chapter8/polymorphism/music/Instrument.java
@@ -0,0 +1,7 @@
+package com.tt.chapter8.polymorphism.music;
+
+public class Instrument {
+    public void play(Note n){
+        System.out.println("Instrument.play()");
+    }
+}
---
+++ src/com/tt/chapter8/polymorphism/music/Note.java
@@ -0,0 +1,5 @@
+package com.tt.chapter8.polymorphism.music;
+
+public enum Note {
+    MIDDLE_C, C_SHARP, B_FLAT;
+}
