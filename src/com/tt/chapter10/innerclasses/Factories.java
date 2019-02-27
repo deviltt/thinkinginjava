e5029ff98a007bd087dc407316790099a514eb28
Administrator <tt416926063@qq.com>
2019-02-27 11:12:57 +0800

    使用匿名内部类实现interfaces/Factories方法

---
+++ src/com/tt/chapter10/innerclasses/Factories.java
@@ -0,0 +1,66 @@
+package com.tt.chapter10.innerclasses;
+
+interface Service{
+    void method1();
+    void method2();
+}
+
+interface ServiceFactory{
+    Service getService();
+}
+
+class Implementation1 implements Service{
+    @Override
+    public void method1() {
+        System.out.println("Implementation1 method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Implementation1 method2");
+    }
+
+    //匿名内部类为实现了ServiceFactory接口的类，并且重写了getService方法
+    public static ServiceFactory factory = new ServiceFactory() {
+        @Override
+        public Service getService() {
+            return new Implementation1();
+        }
+    };
+}
+
+class Implementation2 implements Service{
+
+    @Override
+    public void method1() {
+        System.out.println("Implementation2 method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Implementation2 method2");
+    }
+
+    public static ServiceFactory factory = new ServiceFactory() {
+        @Override
+        public Service getService() {
+            return new Implementation2();
+        }
+    };
+}
+
+public class Factories {
+    /**
+     * 方法功能：调用不同service的method1、method2方法
+     * @param factory
+     */
+    public static void ServiceConsumer(ServiceFactory factory){
+        Service s = factory.getService();
+        s.method1();
+        s.method2();
+    }
+    public static void main(String[] args) {
+        ServiceConsumer(Implementation1.factory);
+        ServiceConsumer(Implementation2.factory);
+    }
+}
