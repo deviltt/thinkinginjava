eac097b82e5a4008f40e482916b6d2cee4345a94
Administrator <tt416926063@qq.com>
2019-02-27 10:48:00 +0800

    接口

---
+++ src/com/tt/chapter10/innerclasses/Contents.java
@@ -0,0 +1,5 @@
+package com.tt.chapter10.innerclasses;
+
+public interface Contents {
+    int value();
+}
---
+++ src/com/tt/chapter10/innerclasses/Destination.java
@@ -0,0 +1,5 @@
+package com.tt.chapter10.innerclasses;
+
+public interface Destination {
+    String readLabel();
+}
