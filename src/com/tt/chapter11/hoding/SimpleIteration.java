e41a5aca9362b407802fb390baca9d9ec4e92904
Administrator <tt416926063@qq.com>
2019-02-28 11:26:26 +0800

    List对象调用iterator();方法返回Iterator对象，这样就可以用foreach方式遍历集合而不用考虑集合元素的数量

---
+++ src/com/tt/chapter11/hoding/SimpleIteration.java
@@ -0,0 +1,28 @@
+package com.tt.chapter11.hoding;
+
+import com.tt.chapter14.typeinfo.pets.Pet;
+import com.tt.chapter14.typeinfo.pets.Pets;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class SimpleIteration {
+    public static void main(String[] args) {
+        List<Pet> pets = Pets.arrayList(12);
+        Iterator<Pet> iterator = pets.iterator();
+        while (iterator.hasNext()){
+            Pet p = iterator.next();
+            System.out.println(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+        for (Pet p : pets)
+            System.out.println(p.id() + ":" + p + " ");
+        System.out.println();
+        iterator = pets.iterator(); //这一步相当于将指针移到pets集合的最前面
+        for (int i = 0; i < 6; i++) {
+            iterator.next();
+            iterator.remove();
+        }
+        System.out.println(pets);
+    }
+}
