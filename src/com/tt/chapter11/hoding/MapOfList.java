32ce4563cea430022574350e6502c5693e2c0012
Administrator <tt416926063@qq.com>
2019-02-28 13:54:48 +0800

    测试了Map.Entry,map.entrySet.iterator();返回的键值对集合的Iterator迭代器

--- src/com/tt/chapter11/hoding/MapOfList.java
+++ src/com/tt/chapter11/hoding/MapOfList.java
@@ -2,10 +2,7 @@ package com.tt.chapter11.hoding;
 
 import com.tt.chapter14.typeinfo.pets.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapOfList {
     private static Map<Person, List<? extends Pet>> petPeople = new HashMap<>();
@@ -18,13 +15,25 @@ public class MapOfList {
     }
 
     public static void main(String[] args) {
-        System.out.println("People: " + petPeople.keySet());
-        System.out.println("Pets " + petPeople.values());
-        for(Person person : petPeople.keySet()){
-            System.out.println(person + " has:");
-            for(Pet pet : petPeople.get(person)){
-                System.out.println(" " + pet);
-            }
+//        System.out.println("People: " + petPeople.keySet());
+//        System.out.println("Pets " + petPeople.values());
+//        for(Person person : petPeople.keySet()){
+//            System.out.println(person + " has:");
+//            for(Pet pet : petPeople.get(person)){
+//                System.out.println(" " + pet);
+//            }
+//        }
+        System.out.println(petPeople);
+        Iterator<Map.Entry<Person, List<? extends Pet>>> iterator = petPeople.entrySet().iterator();
+//        for (Map.Entry entry : petPeople.entrySet()){
+//            System.out.println(entry.getKey());
+//            System.out.println(entry.getValue());
+//        }
+        while (iterator.hasNext()){
+            Map.Entry entry = iterator.next();
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
         }
+//            System.out.println(iterator.next().getKey());
     }
 }
