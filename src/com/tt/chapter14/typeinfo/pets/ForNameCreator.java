967e36aa77e235b0928e58531022da15badce4fc
Administrator <tt416926063@qq.com>
2019-02-28 09:40:06 +0800

    通过类加载的方式创建了一个集合，里面包含了所有Pet的Class对象

--- src/com/tt/chapter14/typeinfo/pets/ForNameCreator.java
+++ src/com/tt/chapter14/typeinfo/pets/ForNameCreator.java
@@ -3,6 +3,9 @@ package com.tt.chapter14.typeinfo.pets;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 通过类加载的方式创建了一个集合，里面包含了所有Pet的Class对象
+ */
 public class ForNameCreator extends PetCreator {
     private static List<Class<? extends Pet>> types = new ArrayList<>();
     private static String[] typeNames = {
