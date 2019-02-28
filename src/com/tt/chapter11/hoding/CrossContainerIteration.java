973d34363594d527634c53e6d08f3bd04f53d7ac
Administrator <tt416926063@qq.com>
2019-02-28 11:57:56 +0800

    迭代器统一了对容器的访问方式

---
+++ src/com/tt/chapter11/hoding/CrossContainerIteration.java
@@ -0,0 +1,26 @@
+package com.tt.chapter11.hoding;
+
+import com.tt.chapter14.typeinfo.pets.Pet;
+import com.tt.chapter14.typeinfo.pets.Pets;
+
+import java.util.*;
+
+public class CrossContainerIteration {
+    public static void main(String[] args) {
+        ArrayList<Pet> pets = Pets.arrayList(8);
+        LinkedList<Pet> pets1 = new LinkedList<>(pets);
+        HashSet<Pet> pets2 = new HashSet<>(pets);
+//        TreeSet<Pet> pets3 = new TreeSet<>(pets);
+        display(pets.iterator());
+        display(pets1.iterator());
+        display(pets2.iterator());
+    }
+
+    private static void display(Iterator<Pet> iterator) {
+        while (iterator.hasNext()){
+            Pet p = iterator.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+}
