79963e38c0831946a84328ca2397080f7711da2f
Administrator <tt416926063@qq.com>
2019-03-04 09:02:46 +0800

    动态代理

--- src/com/tt/chapter14/typeinfo/SimpleDynamicProxy.java
+++ src/com/tt/chapter14/typeinfo/SimpleDynamicProxy.java
@@ -6,15 +6,21 @@ import java.lang.reflect.Proxy;
 
 class DynamicProxyHandler implements InvocationHandler{
     private Object proxied;
+
     public DynamicProxyHandler(Object proxied) {
         this.proxied = proxied;
     }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("**** proxy: " + proxy.getClass() + ", method: " + method + ", args: " + args);
-        if(args != null)
-            for (Object arg : args)
-                System.out.println(" " + arg);
+//        System.out.println("**** proxy: " + proxy.getClass() + ", method: " + method + ", args: " + args);
+//        if (args != null){
+//            for (Object arg : args)
+//                System.out.println(" " + arg);
+//        }
+        if (method.getName().equals("doSomething")){
+            System.out.println("doSomething Interesting");
+        }
         return method.invoke(proxied, args);
     }
 }
@@ -22,14 +28,14 @@ class DynamicProxyHandler implements InvocationHandler{
 public class SimpleDynamicProxy {
     public static void consumer(Interface iface){
         iface.doSomething();
-        iface.somethingElse("banana");
+        iface.somethingElse("bonobo");
     }
-
     public static void main(String[] args) {
+        //对这个realObject对象进行动态代理
         RealObject realObject = new RealObject();
         consumer(realObject);
-
-        Interface proxy = (Interface)Proxy.newProxyInstance(Interface.class.getClassLoader(), new Class[]{Interface.class}, new DynamicProxyHandler(realObject));
+        Interface proxy = (Interface) Proxy.newProxyInstance(Interface.class.getClassLoader()
+        , new Class[]{Interface.class}, new DynamicProxyHandler(realObject));
         consumer(proxy);
     }
 }
