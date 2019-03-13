c96a171f6e28e58878371addf7c3344322f5a4c7
deviltt <tt416926063@qq.com>
2019-03-13 21:58:11 +0800

    使用尾插法来反转单链表

---
+++ src/com/leetcode/listnode/Solution206.java
@@ -0,0 +1,24 @@
+package com.leetcode.listnode;
+
+import com.leetcode.ListNode;
+
+/**
+ * 反转一个单链表。
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+public class Solution206 {
+    public ListNode reverseList(ListNode head) {
+        //使用尾插法
+        ListNode temp = null;
+        ListNode p = null;
+        while (head != null) {
+            p = head.next;
+            head.next = temp;
+            temp = head;
+            head = p;
+        }
+        return temp;
+    }
+}
