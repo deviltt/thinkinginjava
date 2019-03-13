3ce647d1540afd5dfa17a6e72e6c3dcc3e4b5cb2
deviltt <tt416926063@qq.com>
2019-03-13 23:31:14 +0800

    删除链表的所有重复元素

---
+++ src/com/leetcode/listnode/Solution83.java
@@ -0,0 +1,25 @@
+package com.leetcode.listnode;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 示例 1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 示例 2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 用一个引用记录当前值，另一个引用向后遍历，直到遇到第一个不同的值为止
+ */
+public class Solution83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
