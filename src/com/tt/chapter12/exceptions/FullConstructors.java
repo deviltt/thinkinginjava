24849245b99286044b66867fbe7112e30df475f8
deviltt <tt416926063@qq.com>
2019-03-28 21:28:19 +0800

    自定义异常类的两个构造器，并且重定向了错误流，将错误信息重定向到控制台中

---
+++ src/com/tt/chapter12/exceptions/FullConstructors.java
@@ -0,0 +1,36 @@
+package com.tt.chapter12.exceptions;
+
+class MyException extends Exception{
+    public MyException() {
+    }
+
+    //有参构造函数，传入的参数会一同显示到错误信息当中去
+    public MyException(String message) {
+        super(message);
+    }
+}
+
+public class FullConstructors {
+    public static void f() throws MyException{
+        System.out.println("Throwing MyException from f()");
+        throw new MyException();
+    }
+
+    public static void g() throws MyException{
+        System.out.println("Throwing MyException from g()");
+        throw new MyException("Originated in g()");
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace();    //如果没有重定向则默认从System.err输出
+        }
+        try {
+            g();
+        } catch (MyException e) {
+            e.printStackTrace(System.out);  //重定向到控制台输出错误流信息
+        }
+    }
+}
