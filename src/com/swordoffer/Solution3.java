196bf3dd495b5cdae478cb5baab07d030f1c49be
deviltt <tt416926063@qq.com>
2019-03-30 23:31:15 +0800

    查看数组中的重复的数字

--- src/com/swordoffer/Solution3.java
+++ src/com/swordoffer/Solution3.java
@@ -1,7 +1,7 @@
 package com.swordoffer;
 
 /**
- * 查看数组中是否有duplication
+ * 查看数组中是否有重复值
  * [2,3,1,0,2,5,3]
  * [1,3,2,0,2,5,3]
  * [3,1,2,0,2,5,3]
@@ -9,11 +9,11 @@ package com.swordoffer;
  * 另外还可以用hashMap的方法
  */
 public class Solution3 {
-    public boolean duplicate(int[] arr, int duplication) {
+    public boolean duplicate(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            //当索引的值与当前索引对应的元素值不相同时
             while (arr[i] != i) {
                 if (arr[i] == arr[arr[i]]) {
-                    duplication = arr[i];
                     return true;
                 }
                 int temp = arr[i];
