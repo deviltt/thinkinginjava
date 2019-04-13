2d4e0d96675e615d58440b65ed4fa3f2b0172946
deviltt <tt416926063@qq.com>
2019-04-13 23:48:20 +0800

    简单插入排序

---
+++ src/com/introductiontoalgorithms/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.introductiontoalgorithms;
+
+/**
+ * 简单插入排序(升序)
+ */
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            //把满足条件的数都后移
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 6, 4, 1};
+        insertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
