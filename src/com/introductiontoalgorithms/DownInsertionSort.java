37950cde2baf02ac34f517d40df39bc5dd4d3ec9
deviltt <tt416926063@qq.com>
2019-04-14 10:45:10 +0800

    简单插入排序(降序排列)

---
+++ src/com/introductiontoalgorithms/DownInsertionSort.java
@@ -0,0 +1,27 @@
+package com.introductiontoalgorithms;
+
+/**
+ * 简单插入排序(降序)
+ */
+public class DownInsertionSort {
+    public static void downInsertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            //什么情况下需要右移数组元素
+            while (j >= 0 && arr[j] < temp) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 6, 4, 1};
+        downInsertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
