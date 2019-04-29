ef52ad2fca27a3075363e4812b82092a29a473db
deviltt <tt416926063@qq.com>
2019-04-29 21:56:32 +0800

    动态规划之等差数列划分，使用dp数组记录当前长度的数组中有多少个等差数列

---
+++ src/com/leetcode/Solution413.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+/**
+ * 如果一个数列至少有三个元素，并且任意两个相邻元素之差相同，则称该数列为等差数列。
+ * A = [1, 2, 3, 4]
+ * 返回: 3, A 中有三个子等差数组: [1, 2, 3], [2, 3, 4] 以及自身 [1, 2, 3, 4]。
+ * 思路：状态转移方程f(n)=f(n-1)+1; 条件是a(n)-a(n-1)=a(n-1)-a(n-2)
+ */
+public class Solution413 {
+    public int numberOfArithmeticSlices(int[] A) {
+        int result = 0;
+        int[] dp = new int[A.length];
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                dp[i] = dp[i - 1] + 1;
+            result += dp[i];
+        }
+        return result;
+    }
+}
