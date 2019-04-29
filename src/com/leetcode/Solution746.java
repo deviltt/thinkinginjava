96f61a7105747673c2b62df87fda1668fde84ce0
deviltt <tt416926063@qq.com>
2019-04-29 11:47:17 +0800

    动态规划之节约空间，不用开辟数组保存每一次动态规划的结果，只需要记录更新之前一次的动态规划结果即可

--- src/com/leetcode/Solution746.java
+++ src/com/leetcode/Solution746.java
@@ -8,13 +8,27 @@ package com.leetcode;
  */
 public class Solution746 {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+//        int[] dp = new int[cost.length];
+//        dp[0] = cost[0];
+//        dp[1] = cost[1];
+//        for (int i = 2; i < cost.length; i++) {
+//            //cost[i]为当前台阶所消耗的体力值
+//            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+//        }
+//        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+        /**
+         * 思路2：思路1中是要开辟一个和cost数组同样长度的数组，
+         * 而在dp数组中，之前用过的值便没有存在的意义，不会再使用到了，
+         * 因此可以只保留i-1, i-2中的小者就行
+         */
+        int a = cost[0];
+        int b = cost[1];
+        int t;
         for (int i = 2; i < cost.length; i++) {
-            //cost[i]为当前台阶所消耗的体力值
-            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+            t = cost[i] + Math.min(a, b);
+            a = b;
+            b = t;
         }
-        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+        return Math.min(a, b);
     }
 }
