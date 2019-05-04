1bf04a41558e3fc9fc5bf0abe90fd5a14583154b
Administrator <tt416926063@qq.com>
2019-05-04 14:41:05 +0800

    不用dp保存上一次的结果，动态规划的空间优化

--- src/com/leetcode/Solution413.java
+++ src/com/leetcode/Solution413.java
@@ -9,12 +9,24 @@ package com.leetcode;
  */
 public class Solution413 {
     public int numberOfArithmeticSlices(int[] A) {
-        int result = 0;
-        int[] dp = new int[A.length];
+//        int result = 0;
+//        int[] dp = new int[A.length];
+//        for (int i = 2; i < A.length; i++) {
+//            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+//                dp[i] = dp[i - 1] + 1;
+//            result += dp[i];
+//        }
+//        return result;
+        int result = 0, cur = 0, old = 0;
         for (int i = 2; i < A.length; i++) {
-            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
-                dp[i] = dp[i - 1] + 1;
-            result += dp[i];
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                cur = old + 1;
+                old = cur;
+            } else {
+                cur = 0;
+                old = 0;
+            }
+            result += cur;
         }
         return result;
     }
