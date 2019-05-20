6c04b154d6b79656b2971a59da8e5b71b518179e
deviltt <tt416926063@qq.com>
2019-05-20 12:41:29 +0800

    使得整数的每个位置的数单独相加，直到小于10为止

---
+++ src/com/leetcode/Solution258.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+/**
+ * 各位相加
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数
+ * 输入: 38
+ * 输出: 2
+ * 解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+ */
+public class Solution258 {
+    public int addDigits(int num) {
+        int sum;
+        while (num > 9) {
+            sum = 0;
+            while (num > 0) {
+                int temp = num % 10;
+                sum += temp;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
