16e0591cb21d3f600c927f2fa834ad082f1287e7
deviltt <tt416926063@qq.com>
2019-05-21 22:53:20 +0800

    双指针法对数组进行奇偶数与索引一一对应

---
+++ src/com/leetcode/Solution922.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+/**
+ * 给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+ * 对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+ * 你可以返回任何满足上述条件的数组作为答案。
+ * 输入：[4,2,5,7]
+ * 输出：[4,5,2,7]
+ * 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ * 思路：双指针法i用于记录偶数位，j用于记录奇数位
+ */
+public class Solution922 {
+    public int[] sortArrayByParityII(int[] A) {
+        int i = 0, j = 1;
+        while (i < A.length && j < A.length) {
+            if (A[i] % 2 == 0) {
+                i += 2;
+            } else {
+                while (A[j] % 2 != 0 && j < A.length) {
+                    j += 2;
+                }
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        return A;
+    }
+}
