b6ee36eee4f388857bcb70b8dcbe00300fcb2d82
deviltt <tt416926063@qq.com>
2019-05-21 20:59:59 +0800

    自定义异常，并在方法中抛出捕获处理

--- src/com/tt/chapter12/exceptions/InheritingException.java
+++ src/com/tt/chapter12/exceptions/InheritingException.java
@@ -1,5 +1,8 @@
 package com.tt.chapter12.exceptions;
 
+/**
+ * 创建了自定义的异常，必须继承已有的异常类
+ */
 class SimpleException extends Exception {
 }
 
@@ -11,6 +14,9 @@ public class InheritingException {
 
     public static void main(String[] args) {
         InheritingException inheritingException = new InheritingException();
+        /**
+         * 在调用f()函数时，会抛出自定义的异常
+         */
         try {
             inheritingException.f();
         } catch (SimpleException e) {
