e085863b15a7b7a77f8b8d96a009352b253630b9
deviltt <tt416926063@qq.com>
2019-05-28 14:02:21 +0800

    类实现了Iterable接口就具有foreach的枚举功能

---
+++ src/com/tt/chapter15/generics/IterableFibonacci.java
@@ -0,0 +1,35 @@
+package com.tt.chapter15.generics;
+
+import java.util.Iterator;
+
+/**
+ * 类实现了Iterable接口就具有foreach的枚举功能
+ */
+public class IterableFibonacci extends Fibonacci implements Iterable<Integer> {
+    private int n;
+
+    public IterableFibonacci(int count) {
+        n = count;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return n > 0;
+            }
+
+            @Override
+            public Integer next() {
+                n--;
+                return IterableFibonacci.this.next();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        for (int i : new IterableFibonacci(18))
+            System.out.print(i + " ");
+    }
+}
