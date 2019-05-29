09da3226fbf3977b1fe409d4e7c27705e8526ac8
deviltt <tt416926063@qq.com>
2019-05-29 20:44:23 +0800

    类名写错了

--- src/com/tt/net/mindview/util/ThreeTuple.java
+++ src/com/tt/net/mindview/util/ThreeTuple.java
@@ -1,9 +1,9 @@
 package com.tt.net.mindview.util;
 
-public class TreeTuple<A, B, C> extends TwoTuple<A, B> {
+public class ThreeTuple<A, B, C> extends TwoTuple<A, B> {
     public final C third;
 
-    public TreeTuple(A a, B b, C c) {
+    public ThreeTuple(A a, B b, C c) {
         super(a, b);
         third = c;
     }
