fe44f0c4b8dc6b5ae0288dd228e1e2ba34b399cc
Administrator <tt416926063@qq.com>
2019-06-19 09:34:03 +0800

    三角形最小路径和：在原List集合中进行修改

---
+++ src/com/leetcode/Solution120.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * 例如，给定三角形：
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ */
+public class Solution120 {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = 0; i < triangle.size(); i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (i>=1){
+                    if (j==0)
+                        triangle.get(i).set(j, triangle.get(i-1).get(0)+triangle.get(i).get(0));
+                    else if (j==i)
+                        triangle.get(i).set(j, triangle.get(i-1).get(j-1)+triangle.get(i).get(j));
+                    else
+                        triangle.get(i).set(j, triangle.get(i).get(j)+ Math.min(triangle.get(i-1).get(j-1), triangle.get(i-1).get(j)));
+                }
+            }
+        }
+        return Collections.min(triangle.get(triangle.size() - 1));
+    }
+
+//    public static void main(String[] args) {
+//        List<List<Integer>> lists=new ArrayList<>();
+//        lists.add()
+//    }
+}
