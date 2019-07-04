e632557fb1cbecc192baf64e2f1d4ee027a93095
Taotao <tt416926063@qq.com>
2019-07-04 16:22:18 +0800

    使用正则表达式匹配数字字符串时候正确

---
+++ src/com/tt/chapter13/strings/IntegerMatch.java
@@ -0,0 +1,10 @@
+package com.tt.chapter13.strings;
+
+public class IntegerMatch {
+    public static void main(String[] args) {
+        System.out.println("-1234".matches("-?\\d+"));  //可能有一个负号，后面跟着一个或多个数字
+        System.out.println("2345".matches("-?\\d+"));
+        System.out.println("+933".matches("-?\\d+"));
+        System.out.println("-122".matches("(-|\\+)?\\d+"));  //可能有一个负号或者正号，并且后面有多个数字
+    }
+}
