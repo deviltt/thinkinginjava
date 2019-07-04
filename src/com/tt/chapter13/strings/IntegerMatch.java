1d799b71af18b198ce5246ad04a1d443cca488fc
Taotao <tt416926063@qq.com>
2019-07-04 16:23:51 +0800

    整型转变为String类型

--- src/com/tt/chapter13/strings/IntegerMatch.java
+++ src/com/tt/chapter13/strings/IntegerMatch.java
@@ -6,5 +6,6 @@ public class IntegerMatch {
         System.out.println("2345".matches("-?\\d+"));
         System.out.println("+933".matches("-?\\d+"));
         System.out.println("-122".matches("(-|\\+)?\\d+"));  //可能有一个负号或者正号，并且后面有多个数字
+        System.out.println(String.valueOf(-234).matches("[-+]?\\d+"));
     }
 }
