c07b1019330f4992c6dd48bcf0a0ce8339f6b3ba
deviltt <tt416926063@qq.com>
2019-07-22 23:27:44 +0800

    没有使用EnumSet和EnumMap的情况下，应该将每个枚举常量的在类型中的位置保存在实例域中

--- src/com/tt/chapter19/enumerated/demo/Ensemble.java
+++ src/com/tt/chapter19/enumerated/demo/Ensemble.java
@@ -1,12 +1,20 @@
 package com.tt.chapter19.enumerated.demo;
 
-import java.util.EnumSet;
-
+/**
+ * 除非使用EnumSet和EnumMap，否则完全避免使用ordinal方法返回
+ * 每个枚举常量在类型中的数字位置，应该将其保存在一个实例域中
+ */
 public enum Ensemble {
-    SOLO, DUET, TRIO, QUARTET;
+    SOLO(1), DUET(2), TRIO(2), QUARTET(3);
+
+    private final int numberOfMusicians;
+
+    Ensemble(int numberOfMusicians) {
+        this.numberOfMusicians = numberOfMusicians;
+    }
 
     public int numberOfMusicians() {
-        return ordinal() + 1;
+        return numberOfMusicians + 1;
     }
 
     public static void main(String[] args) {
