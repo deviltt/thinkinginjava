c2adfb0359c93d79280699919f4c738837ea80df
deviltt <tt416926063@qq.com>
2019-07-31 18:19:33 +0800

    Callable和Runnable的区别

--- src/com/tt/chapter21/concurrency/CallableDemo.java
+++ src/com/tt/chapter21/concurrency/CallableDemo.java
@@ -18,14 +18,16 @@ class TaskWithResult implements Callable<String>{
 
 public class CallableDemo {
     public static void main(String[] args) {
+        //创建一个线程池
         ExecutorService executorService = Executors.newCachedThreadPool();
+        //List集合存放Future对象
         ArrayList<Future<String>> results = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             results.add(executorService.submit(new TaskWithResult(i)));
         }
         for (Future<String> future : results){
             try {
-                System.out.println(future.get());
+                System.out.println(future.get());   //Future对象的get方法，返回call方法返回的字符串
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
