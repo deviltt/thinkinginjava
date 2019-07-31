bb8de31cd6467f47928d53ab8ae59531c1ac093f
deviltt <tt416926063@qq.com>
2019-07-31 18:30:38 +0800

    线程的sleep方法将阻塞线程一段时间(设定的时间)

---
+++ src/com/tt/chapter21/concurrency/SleepingTask.java
@@ -0,0 +1,27 @@
+package com.tt.chapter21.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SleepingTask extends LiftOff {
+    @Override
+    public void run() {
+        try {
+            while (countDown-- > 0) {
+                System.out.println(status());
+                TimeUnit.MILLISECONDS.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService= Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new SleepingTask());
+        }
+        executorService.shutdown();
+    }
+}
