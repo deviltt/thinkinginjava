7063f83bf979058bae5f37cb95344885171054b3
Taotao <tt416926063@qq.com>
2019-12-26 10:53:49 +0800

    162. 寻找峰值（方法2）

---
+++ src/com/leetcodespecialtopic/binarysearch/Solution162ed.java
@@ -0,0 +1,28 @@
+package com.leetcodespecialtopic.binarysearch;
+
+/**
+ * 162. 寻找峰值（方法2）
+ * 峰值元素是指其值大于左右相邻值的元素。
+ * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+ * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ * 你可以假设 nums[-1] = nums[n] = -∞。
+ * 输入: nums = [1,2,3,1]
+ * 输出: 2
+ * 解释: 3 是峰值元素，你的函数应该返回其索引 2。
+ */
+public class Solution162ed {
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+
+    private int search(int[] nums, int left, int right) {
+        if (left == right) {
+            return left;
+        }
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[mid + 1]) {
+            return search(nums, left, mid);
+        }
+        return search(nums, mid + 1, right);
+    }
+}
