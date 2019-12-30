03f0ae047a2bc25cf525d5233f107d8f48c026a4
deviltt <tt416926063@qq.com>
2019-12-30 22:43:40 +0800

    102. 二叉树的层次遍历

---
+++ src/com/leetcodespecialtopic/binarytree/Solution102.java
@@ -0,0 +1,49 @@
+package com.leetcodespecialtopic.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层次遍历
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+public class Solution102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < queueSize; i++) {
+                if (queue.peek().left != null) {
+                    queue.add(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.add(queue.peek().right);
+                }
+                list.add(queue.poll().val);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
