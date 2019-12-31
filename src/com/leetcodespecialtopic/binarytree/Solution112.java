69cd4aa95c98b28e33ecf746361b213ffac2e80b
deviltt <tt416926063@qq.com>
2019-12-31 23:37:00 +0800

    112. 路径总和

---
+++ src/com/leetcodespecialtopic/binarytree/Solution112.java
@@ -0,0 +1,33 @@
+package com.leetcodespecialtopic.binarytree;
+
+/**
+ * 112. 路径总和
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ */
+public class Solution112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return pathSum(root, sum);
+    }
+
+    private boolean pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        //或的原因是只要有一个满足条件，则返回true
+        return pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val);
+    }
+}
