dfb22490450fd4767f01c1624a90d3adcc1fc53a
Taotao <tt416926063@qq.com>
2019-12-31 13:45:22 +0800

    101. 对称二叉树

---
+++ src/com/leetcodespecialtopic/binarytree/Solution101ed.java
@@ -0,0 +1,47 @@
+package com.leetcodespecialtopic.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101. 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * 关键是如何表达出 对称 的概念
+ */
+public class Solution101ed {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+            if (node1.val != node2.val) {
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
+    }
+}
