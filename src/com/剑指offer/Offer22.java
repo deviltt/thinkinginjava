0b6ea6194fe8c878f74f8e00c3ec04d4465a696a
Taotao <tt416926063@qq.com>
2020-06-23 18:29:15 +0800

    链表中倒数第k个数

---
+++ src/com/剑指offer/Offer22.java
@@ -0,0 +1,21 @@
+package com.剑指offer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 用list来保存遍历的元素
+ */
+public class Offer22 {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        List<ListNode> list = new ArrayList<>();
+
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+
+        return list.get(list.size()-k);
+    }
+}
---
+++ src/com/剑指offer/Offer22ed.java
@@ -0,0 +1,23 @@
+package com.剑指offer;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 双指针法
+ */
+public class Offer22ed {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode cur = head, next = head;
+
+        while (k > 1) {
+            next = next.next;
+            --k;
+        }
+
+        while (next.next != null) {
+            cur = cur.next;
+            next = next.next;
+        }
+
+        return cur;
+    }
+}
