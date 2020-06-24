dfd6aaee447ff29c5137b30e9d3d4c4074030c5d
Taotao <tt416926063@qq.com>
2020-06-24 13:58:31 +0800

    剑指 Offer 24. 反转链表

---
+++ src/com/剑指offer/Offer24.java
@@ -0,0 +1,24 @@
+package com.剑指offer;
+
+/**
+ * 剑指 Offer 24. 反转链表
+ */
+public class Offer24 {
+    private ListNode dummyHead = new ListNode(-1);
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head;
+
+        while (cur != null) {
+            head = head.next;
+            cur.next = dummyHead.next;
+            dummyHead.next = cur;
+            cur = head;
+        }
+
+        return dummyHead.next;
+    }
+}
