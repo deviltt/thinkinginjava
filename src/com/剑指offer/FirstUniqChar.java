dcb2373d2bd13b0e3960c062653ccaf6099a3aaf
Taotao <tt416926063@qq.com>
2020-07-01 18:04:09 +0800

    剑指 Offer 50. 第一个只出现一次的字符

---
+++ src/com/剑指offer/FirstUniqChar.java
@@ -0,0 +1,36 @@
+package com.剑指offer;
+
+import java.util.HashMap;
+
+/**
+ * 剑指 Offer 50. 第一个只出现一次的字符
+ */
+
+public class FirstUniqChar {
+    public static char firstUniqChar(String s) {
+        if (s.length() == 0) {
+            return ' ';
+        }
+        HashMap<Character, Boolean> map=new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char temp=s.charAt(i);
+            if (!map.containsKey(temp)){
+                map.put(temp, true);
+            }else{
+                map.put(temp, false);
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char temp=s.charAt(i);
+            if (map.get(temp)){
+                return temp;
+            }
+        }
+        return ' ';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+    }
+}
