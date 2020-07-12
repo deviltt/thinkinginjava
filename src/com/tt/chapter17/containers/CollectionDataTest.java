188f25a037e336d47d6f1a9b5df5626c5dd0d9dc
deviltt <tt416926063@qq.com>
2020-07-12 18:05:42 +0800

    填充容器

--- src/com/tt/chapter17/containers/CollectionDataTest.java
+++ src/com/tt/chapter17/containers/CollectionDataTest.java
@@ -22,7 +22,13 @@ class Government implements Generator<String> {
 public class CollectionDataTest {
     public static void main(String[] args) {
         Set<String> set = new LinkedHashSet<>(new CollectionData<>(new Government(), 15));
+        //CollectionData.list()会返回一个继承了ArrayList的子类集合对象，这个集合对象已经填充了n个String
         set.addAll(CollectionData.list(new Government(), 15));
         System.out.println(set);
+        System.out.println(set.size());
     }
 }
+
+//打印结果
+//[Strange, women, lying, in, ponds, distributing, swords, is, no, basis, for, a, system, of, government]
+//15
