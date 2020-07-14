eeaf6cda57db56f869afb7742516b3c10754d112
Taotao <tt416926063@qq.com>
2020-07-14 13:47:19 +0800

    在运行时使用类型信息

--- src/com/tt/chapter14/typeinfo/toys/ToyTest.java
+++ src/com/tt/chapter14/typeinfo/toys/ToyTest.java
@@ -15,6 +15,10 @@ class FancyToy extends Toy implements HasBatteries, Waterproof, Shoots{
     }
 }
 
+/**
+ * classObj.getName()和classObj.getCanonicalName()都是获取类的全限定名
+ * classObj.getSimpleName()获取不含包名的类名
+ */
 public class ToyTest {
     static void printInfo(Class c){
         System.out.println("Class name: " + c.getName() + " is interface? [" + c.isInterface() + "]");
@@ -25,23 +29,23 @@ public class ToyTest {
     public static void main(String[] args) {
         Class c = null;
         try {
-            c = Class.forName("com.tt.chapter14.typeinfo.toys.FancyToy");
+            c = Class.forName("com.tt.chapter14.typeinfo.toys.FancyToy");   //获取FancyToy的class对象
         }catch (ClassNotFoundException e){
             System.out.println("can't find FancyToy");
             System.exit(1);
         }
-        printInfo(c);
-        for (Class face : c.getInterfaces())
-            printInfo(face);
-        Class up = c.getSuperclass();
+        printInfo(c);   //打印这个class对象的类型信息
+        for (Class face : c.getInterfaces())    //获取class对象所有实现的接口
+            printInfo(face);    //打印每个接口的类型信息
+        Class up = c.getSuperclass();   //获取父类class对象
         Object object = null;
         try {
-            object = up.newInstance();
+            object = up.newInstance();  //用class创建类对象，必须使用无参构造器
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        printInfo(object.getClass());
+        printInfo(object.getClass());   //获取对象的class对象
     }
 }
