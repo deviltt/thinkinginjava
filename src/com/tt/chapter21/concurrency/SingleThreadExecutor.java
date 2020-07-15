59524a9f6c4c2225b512b827c9f001dd0393959f
deviltt <tt416926063@qq.com>
2020-07-15 22:34:08 +0800

    利用线程池

--- src/com/tt/chapter21/concurrency/SingleThreadExecutor.java
+++ src/com/tt/chapter21/concurrency/SingleThreadExecutor.java
@@ -3,6 +3,10 @@ package com.tt.chapter21.concurrency;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * newSingleThreadExecutor
+ * 每次只会有一个线程执行，执行完后，放在队列的最后面
+ */
 public class SingleThreadExecutor {
     public static void main(String[] args) {
         //SingleThreadExecutor确保任意时刻在任何县城中都只有唯一的任务在运行
