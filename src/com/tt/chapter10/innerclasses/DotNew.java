bc7311b42fb309587e564bb3f429b4cbd17e6df9
deviltt <tt416926063@qq.com>
2020-07-19 21:42:52 +0800

    .new和.class的用法

--- src/com/tt/chapter10/innerclasses/DotNew.java
+++ src/com/tt/chapter10/innerclasses/DotNew.java
@@ -2,6 +2,8 @@ package com.tt.chapter10.innerclasses;
 
 /**
  * 使用外部类对象创建内部类对象
+ * 外部类对象的引用创建内部类对象 outerObj.new InnerClassName();
+ * .new 的用法
  */
 public class DotNew {
     public class Inner{}
--- src/com/tt/chapter10/innerclasses/DotThis.java
+++ src/com/tt/chapter10/innerclasses/DotThis.java
@@ -1,10 +1,17 @@
 package com.tt.chapter10.innerclasses;
 
+/**
+ * 返回对外部类的引用 外部类名字.this
+ */
 public class DotThis {
     void f(){
         System.out.println("DotThis.f()");
     }
     public class Inner{
+        /**
+         * 返回对外部类的引用 外部类名字.this ：DotThis.this
+         * @return
+         */
         public DotThis outer(){
             return DotThis.this;
         }
@@ -15,7 +22,7 @@ public class DotThis {
 
     public static void main(String[] args) {
         DotThis dotThis = new DotThis();    //创建一个外部类对象
-        DotThis.Inner inner = dotThis.inner();
+        DotThis.Inner inner = dotThis.inner();  //生成内部类对象 OuterClassName.InnerClassName
         inner.outer().f();  //使用内部类方法返回外部类对象，调用外部类中的f();方法
     }
 }
