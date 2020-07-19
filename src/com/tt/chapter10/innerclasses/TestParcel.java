e06fc385faf95daa3380281eeccc1d0ae61a918b
deviltt <tt416926063@qq.com>
2020-07-19 22:35:05 +0800

    内部类对象向上转型

---
+++ src/com/tt/chapter10/innerclasses/TestParcel.java
@@ -0,0 +1,43 @@
+package com.tt.chapter10.innerclasses;
+
+class Parcel4 {
+    private class PContents implements Contents {
+        private int i = 11;
+
+        @Override
+        public int value() {
+            return i;
+        }
+    }
+
+    protected class PDestination implements Destination {
+        private String label;
+
+        private PDestination(String whereTo) {
+            label = whereTo;
+        }
+
+        @Override
+        public String readLabel() {
+            return label;
+        }
+    }
+
+    public Destination destination(String s) {
+        return new PDestination(s);
+    }
+
+    public Contents contents() {
+        return new PContents();
+    }
+}
+
+public class TestParcel {
+    public static void main(String[] args) {
+        Parcel4 p = new Parcel4();
+        Contents c = p.contents();
+        Parcel4.PDestination d = (Parcel4.PDestination) p.destination("Tasmania");
+
+//        Parcel4.PContents 私有内部类无法在其它类中使用
+    }
+}
