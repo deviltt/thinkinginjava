3451245958860bcdc05907ad8d2c1a1226abcca4
deviltt <tt416926063@qq.com>
2020-07-19 22:09:11 +0800

    局部内部类：类似于js闭包的概念

--- src/com/tt/chapter10/innerclasses/Parcel5.java
+++ src/com/tt/chapter10/innerclasses/Parcel5.java
@@ -1,6 +1,14 @@
 package com.tt.chapter10.innerclasses;
 
+/**
+ * 局部内部类：类似于js闭包的概念
+ */
 public class Parcel5 {
+    /**
+     * 返回了一个对局部内部类对象的一个引用
+     * @param s 传递参数
+     * @return 将局部内部类向上转型
+     */
     public Destination destination(String s){
         class PDestination implements Destination{
             private String label;
@@ -11,7 +19,7 @@ public class Parcel5 {
 
             @Override
             public String readLabel() {
-                return label;
+                return label+" tt";
             }
         }
         return new PDestination(s);
@@ -19,6 +27,7 @@ public class Parcel5 {
 
     public static void main(String[] args) {
         Parcel5 parcel5 = new Parcel5();
-        Destination destination = parcel5.destination("Tasmania");
+        Destination destination = parcel5.destination("Tasmania");  //相当于闭包的概念，虽然
+        System.out.println(destination.readLabel());
     }
 }
