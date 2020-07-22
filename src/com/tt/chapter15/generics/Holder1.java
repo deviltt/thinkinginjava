18bac325403cef60435de6822649cd4c737d43d9
deviltt <tt416926063@qq.com>
2020-07-22 21:34:43 +0800

    15.2 简单泛型

---
+++ src/com/tt/chapter15/generics/Holder1.java
@@ -0,0 +1,17 @@
+package com.tt.chapter15.generics;
+
+class Automobile {
+
+}
+
+public class Holder1 {
+    private Automobile a;
+
+    public Holder1(Automobile a) {
+        this.a = a;
+    }
+
+    Automobile get() {
+        return a;
+    }
+}
