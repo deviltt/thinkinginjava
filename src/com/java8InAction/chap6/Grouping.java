3054abb1538d66396d559d03651c2234af5d925e
deviltt <tt416926063@qq.com>
2020-08-03 23:05:15 +0800

    多级分组

---
+++ src/com/java8InAction/chap6/Grouping.java
@@ -0,0 +1,83 @@
+package com.java8InAction.chap6;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.java8InAction.chap6.Dish.menu;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
+
+public class Grouping {
+    enum CaloricLevel {DIET, NORMAL, FAT}
+
+    ;
+
+    public static void main(String[] args) {
+        System.out.println("Dishes grouped by type: " + groupDishesByType());
+//        System.out.println("Dish names grouped by type: " + groupDishNamesByType());
+//        System.out.println("Dish tags grouped by type: " + groupDishTagsByType());
+//        System.out.println("Caloric dishes grouped by type: " + groupCaloricDishesByType());
+        System.out.println("Dishes grouped by caloric level: " + groupDishesByCaloricLevel());
+        System.out.println("Dishes grouped by type and caloric level: " + groupDishedByTypeAndCaloricLevel());
+        System.out.println("Count dishes in groups: " + countDishesInGroups());
+        System.out.println("Most caloric dishes by type: " + mostCaloricDishesByType());
+        System.out.println("Most caloric dishes by type: " + mostCaloricDishesByTypeWithoutOprionals());
+        System.out.println("Sum calories by type: " + sumCaloriesByType());
+//        System.out.println("Caloric levels by type: " + caloricLevelsByType());
+    }
+
+    //直接按照类型分组
+    private static Map<Dish.Type, List<Dish>> groupDishesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType));
+    }
+
+    private static Map<CaloricLevel, List<Dish>> groupDishesByCaloricLevel() {
+        return menu.stream().collect(groupingBy(dish -> {
+            if (dish.getCalories() <= 400) {
+                return CaloricLevel.DIET;
+            } else if (dish.getCalories() <= 700) {
+                return CaloricLevel.NORMAL;
+            } else {
+                return CaloricLevel.FAT;
+            }
+        }));
+    }
+
+    //多级分组
+    private static Map<Dish.Type, Map<CaloricLevel, List<Dish>>> groupDishedByTypeAndCaloricLevel() {
+        return menu.stream().collect(groupingBy(Dish::getType,
+                groupingBy(dish -> {
+                    if (dish.getCalories() <= 400) {
+                        return CaloricLevel.DIET;
+                    } else if (dish.getCalories() <= 700) {
+                        return CaloricLevel.NORMAL;
+                    } else {
+                        return CaloricLevel.FAT;
+                    }
+                })));
+    }
+
+    //对分组进行计数
+    private static Map<Dish.Type, Long> countDishesInGroups() {
+        return menu.stream().collect(groupingBy(Dish::getType, counting()));
+    }
+
+    private static Map<Dish.Type, Optional<Dish>> mostCaloricDishesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType,
+//                reducing((Dish d1, Dish d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)));
+                maxBy(comparingInt(Dish::getCalories))));
+    }
+
+    private static Map<Dish.Type, Dish> mostCaloricDishesByTypeWithoutOprionals() {
+        return menu.stream()
+                .collect(groupingBy(Dish::getType,
+                        collectingAndThen(maxBy(comparingInt(Dish::getCalories)), Optional::get)));
+    }
+
+    private static Map<Dish.Type, Integer> sumCaloriesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType,
+                summingInt(Dish::getCalories)));
+    }
+}
