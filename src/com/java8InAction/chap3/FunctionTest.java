120e84f4c6ffb51905afc1076e8e4ac448bf8599
deviltt <tt416926063@qq.com>
2020-08-04 23:21:46 +0800

    行为参数化

---
+++ src/com/java8InAction/chap3/FunctionTest.java
@@ -0,0 +1,21 @@
+package com.java8InAction.chap3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionTest {
+    public static void main(String[] args) {
+        List<Integer> l= map(Arrays.asList("lambdas","in","action"), (String s) -> s.length());
+        System.out.println(l);
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> f) {
+        List<R> result = new ArrayList<>();
+        for (T s : list) {
+            result.add(f.apply(s));
+        }
+        return result;
+    }
+}
