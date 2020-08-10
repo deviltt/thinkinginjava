bed0b36889bbbada369cf201de458012d2483547
deviltt <tt416926063@qq.com>
2020-08-10 22:32:12 +0800

    实现Collector

---
+++ src/com/java8InAction/chap6/ToListCollector.java
@@ -0,0 +1,44 @@
+package com.java8InAction.chap6;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collector.Characteristics.*;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return () -> new ArrayList<>();
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return (list, item) -> list.add(item);
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        //    static <T> Function<T, T> identity() {
+        //        return t -> t;
+        //    }
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH, CONCURRENT));
+    }
+}
