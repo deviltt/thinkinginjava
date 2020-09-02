1c8ff431088910f0d8fb05dddd5e17321aa1b8d3
deviltt <tt416926063@qq.com>
2020-09-02 09:26:11 +0800

    剑指 Offer 15. 二进制中1的个数 添加注释

--- src/com/剑指offer/Offer15.java
+++ src/com/剑指offer/Offer15.java
@@ -3,14 +3,11 @@ package com.剑指offer;
 public class Offer15 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int result = 0;
-        int mask = 1;
-        for (int i = 0; i < 32; i++) {
-            if ((n & mask) != 0) {
-                ++result;
-            }
-            mask <<= 1;
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n = n >>> 1;    //无论正负数，右移后高位都补0
         }
-        return result;
+        return count;
     }
 }
--- src/com/剑指offer/Offer15ed.java
+++ src/com/剑指offer/Offer15ed.java
@@ -1,5 +1,10 @@
 package com.剑指offer;
 
+/**
+ * 剑指 Offer 15. 二进制中1的个数
+ * 巧用 n & (n-1)
+ * n & (n-1) 的结果是 当前1右边与的结果都是0，左边所有的1个数和位置都保持不变
+ */
 public class Offer15ed {
     public int hammingWeight(int n) {
         int result = 0;
