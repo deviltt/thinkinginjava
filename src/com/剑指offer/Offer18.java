b37185ab71994ea2094a5be1e6c99c55e65a2745
deviltt <tt416926063@qq.com>
2020-09-02 18:27:11 +0800

    剑指 Offer 18. 删除链表的节点(优化)

--- src/com/剑指offer/Offer18.java
+++ src/com/剑指offer/Offer18.java
@@ -1,5 +1,9 @@
 package com.剑指offer;
 
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * 创建dummyHead保留prev
+ */
 public class Offer18 {
     public ListNode deleteNode(ListNode head, int val) {
         ListNode dummyHead = new ListNode(-1);
@@ -7,12 +11,12 @@ public class Offer18 {
         dummyHead.next = head;
         ListNode cur = head;
         while (cur != null) {
-            if (cur.val == val) {
-                p.next = cur.next;
-                cur.next = null;
+            if(cur.val==val){
+                p.next=cur.next;
+            }else{
+                p=cur;
             }
-            p = cur;
-            cur = cur.next;
+            cur=cur.next;
         }
         return dummyHead.next;
     }
