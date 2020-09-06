82ce9d6cc02d9a7ed76a78f056561dba4af3ff6f
deviltt <tt416926063@qq.com>
2020-09-06 11:19:33 +0800

    剑指 Offer 20. 表示数值的字符串(添加注释)

--- src/com/剑指offer/Offer20.java
+++ src/com/剑指offer/Offer20.java
@@ -1,8 +1,17 @@
 package com.剑指offer;
 
+/**
+ * 剑指 Offer 20. 表示数值的字符串
+ * 遍历每一个字符，具体有以下几种情况
+ * 1.如果是数字，那么numExist置位true；
+ * 2.如果小数点，小数点前不允许有 dot 或者 e
+ * 3.如果是e || E，之前不允许有e或者E，且前面必须要有数字(前面可以是小数点)
+ * 4.如果是+ || -，符号位在第一位或者前面一位必须是e或者E(必须)
+ * 5.其它字符一律返回false
+ */
 public class Offer20 {
     public boolean isNumber(String s) {
-        if (s==null||s.length()==0){
+        if (s == null || s.length() == 0) {
             return false;
         }
         char[] chars = s.trim().toCharArray();
@@ -19,7 +28,7 @@ public class Offer20 {
                 dotExists = true;
             } else if (c == 'e' || c == 'E') {
                 //e/E之前不能有e/E，且前面必须要有数字
-                if (eExists && !numExists) {
+                if (eExists || !numExists) {
                     return false;
                 }
                 numExists = false;
